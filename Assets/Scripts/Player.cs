d72fa516831015627d1af61ec8ecbff1006ffc98
quagsirus <quagsirus@catpowered.net>
2023-06-19 23:50:01 +0100

    start work on player script

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,18 +1,65 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // Constant modifiers
+    private const float Speed = 1f;
+
+    // Animator and parameters
+    private static readonly int AnimatorDirection = Animator.StringToHash("Direction");
+
+    private Animator _animator;
+
+    // Facing direction for use in animation
+    private Direction _direction = Direction.Down;
+
+    private Direction CurrentDirection
     {
-        
+        get => _direction;
+        set
+        {
+            if (_direction == value) return;
+            _direction = value;
+            OnDirectionChanged(value);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        _animator = gameObject.GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        // Get player inputs and create vector
+        var inputDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        // Clamp to length of 1 otherwise diagonal input would be 40% faster
+        var finalMovement = Vector2.ClampMagnitude(inputDirection, 1.0f);
+
+        CurrentDirection = Vector2.SignedAngle(Vector2.up, finalMovement) switch
+        {
+            < -135 or > 135 => Direction.Up,
+            >= 45 => Direction.Right,
+            <= -45 => Direction.Left,
+            _ => Direction.Down
+        };
+
+        // Avoid unnecessary calculations
+        if (finalMovement != Vector2.zero)
+            // Actually move, accounting for frame times and walk speed
+            transform.Translate((Vector3)finalMovement * (Time.deltaTime * Speed), Space.World);
+    }
+
+    private void OnDirectionChanged(Direction direction)
+    {
+        _animator.SetInteger(AnimatorDirection, (int)direction);
+    }
+
+    private enum Direction
+    {
+        Up,
+        Down,
+        Left,
+        Right
     }
 }
