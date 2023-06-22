04bb373fdd8cbe462a5aa814e92e2c6ae9e9c3a5
Idle-Pigeon <emmie.lily789@gmail.com>
2023-06-22 23:22:34 +0100

    added cat

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,7 +5,7 @@ public class MainMenu : MonoBehaviour
 {
     public void StartGame()
     {
-        SceneManager.LoadScene("Level1");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     public void QuitGame()
